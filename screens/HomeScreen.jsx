059d2c7e6aa78840afd8de5fc75723350168db85
Vladimír Macoun <vlamaco019@pslib.cz>
2023-03-24 11:47:15 +0100

    GPS sensors 1

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,9 +1,62 @@
 import React, { useState } from 'react';
 import { Text, View, ScrollView, TextInput, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import Geolocation from 'react-native-geolocation-service';
+import { PermissionsAndroid } from 'react-native';
+
 
 export const HomeScreen = ({navigation}) => {
   const [vstupPocasko, setVstupPocasko] = useState("")
+  const [location, setLocation] = useState(null);
+
+  // Function to get permission for location
+const requestLocationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Geolocation Permission',
+        message: 'Can we access your location?',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    console.log('granted', granted);
+    if (granted === 'granted') {
+      console.log('You can use Geolocation');
+      return true;
+    } else {
+      console.log('You cannot use Geolocation');
+      return false;
+    }
+  } catch (err) {
+    return false;
+  }
+};
+
+// function to check permissions and get Location
+const getLocation = () => {
+  const result = requestLocationPermission();
+  result.then(res => {
+    console.log('res is:', res);
+    if (res) {
+      Geolocation.getCurrentPosition(
+        position => {
+          console.log(position);
+          setLocation(position);
+        },
+        error => {
+          // See error code charts below.
+          console.log(error.code, error.message);
+          setLocation(false);
+        },
+        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+      );
+    }
+  });
+  console.log(location);
+};
 
   const handleNavigationInput = () => {
     console.log(vstupPocasko);
@@ -20,7 +73,7 @@ export const HomeScreen = ({navigation}) => {
                 <Ionicons name="search-outline" size={24} color="#AAAAAA" />
                 </TouchableOpacity>
             </View>
-            <TouchableOpacity style={styles.gpsButton}>
+            <TouchableOpacity style={styles.gpsButton} onPress={getLocation}>
                 <Text style={styles.gpsButtonText}>Use GPS instead</Text>
             </TouchableOpacity>
     </ScrollView>
